4fc29a548e6e562809da071c23af5ff6c02aabd8
RamTaja4163 <ramgk40@gmail.com>
2022-03-07 12:32:10 +0530

    'hello'

---
+++ function.py
@@ -0,0 +1,37 @@
+# def multiply():
+#     result = 10 * 4
+#     return result
+#
+# answer = multiply()
+# print(answer)
+#
+
+
+# def multiply(x,y):
+#     result = x * y
+#     return result
+#
+# answer = multiply(9,4)
+# print(answer)
+#
+# forty_two = multiply(6,7)
+# print(forty_two)
+#
+# print()
+#
+# for val in range(1,5):
+#     two_items = multiply(2,val)
+#     print(two_items)
+
+fruit = {"orange": "a sweet orange citrus fruit",
+         "apple": "good for making cider",
+         "lemon": "a sour yellow citrus fruit",
+         "grape": "a small fruit growing in bunches",
+         "lime": "a sour green citrus fruit"}
+print(fruit)
+print(fruit["lemon"])
+del fruit["lemon"]
+print(fruit)
+
+
+
